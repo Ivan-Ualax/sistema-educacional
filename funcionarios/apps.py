6169ec40af1727ee026cfb1dc33772bf61098cc4
Ivan Machado <ivanmendesmachado@gmail.com>
2026-05-22 07:39:10 -0300

    refatora signals e remove create_admin do ready

--- funcionarios/apps.py
+++ funcionarios/apps.py
@@ -1,13 +1,8 @@
 from django.apps import AppConfig
 
-
 class FuncionariosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'funcionarios'
 
     def ready(self):
-        try:
-            from .scripts import create_admin
-            create_admin.run()
-        except Exception as e:
-            print("Erro ao criar admin:", e)
+        import funcionarios.signals
